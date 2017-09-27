d4fd9bac55b9009f1bc8b7c1bf885d456dd715cc
takuzuka@mail.ru <takuzuka@mail.ru>
2017-09-27 14:08:32 +0300

    20170927_14_10

---
+++ shell_example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#3. piemers
+NAME="Gadya Petrovic"
+echo $NAME
+unset NAME
+echo $NAME
+
+#2. piemers
+#NAME="Gadya Petrovic"
+#readonly NAME
+#echo $NAME
+#NAME="Gadya Petrovich Hrenova"
+#echo $NAME
+
+#1. piemers
+#NAME="Gadya Petrovic"
+#echo $NAME
+
+#0. piemers
+#history > history_20170927.txt
