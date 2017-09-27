c6c983facdd752c747843695ab1d5c10caad6c76
takuzuka@mail.ru <takuzuka@mail.ru>
2017-09-27 16:08:52 +0300

    20170927_16_10

--- shell_example.sh
+++ shell_example.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
 
+#7.piemers
+vertiba=`expr 2 + 2`
+echo "summa karoch: $vertiba "
+echo "summa karoch:" $vertiba
+#6. piemers
+#echo $*
+#echo "............"
+#kaartas_numurs=1
+#for arguments in $*
+#do
+#	echo $kaartas_numurs". arguments- " $arguments
+#	kaartas_numurs=$kaartas_numurs+1
+#done
+
+#5. piemers
+#echo "izpildama skripta faila nosaukums:" $0
+#echo "SKriptam nodoto argumenru skaits:" $#
+#echo "Argumentu saraksts ( attelosana/ grupesana veids 1):" $*
+#echo "Argumentu saraksts ( attelosana/ grupesana veids 2):" $@
+#echo "pirma argumenta vertiba" $1
+#echo "otra argumenta vertiba" $2
+#echo $1$2
+
+#4. piemers
+#echo "izpildama skripta faila nosaukums:" $0
+##echo $n
+#echo "SKriptam nodoto argumenru skaits:" $#
+#echo "Argumentu saraksts ( attelosana/ grupesana veids 1):" $*
+#echo "Argumentu saraksts ( attelosana/ grupesana veids 2):" $@
+#echo "ieprieksejas komandas izpildes rezultats:" $?
+#echo "skripta izpildei pieskirtais procesa numurs:" $$
+#echo $!
+
 #3. piemers
-NAME="Gadya Petrovic"
-echo $NAME
-unset NAME
-echo $NAME
+#NAME="Gadya Petrovic"
+#echo $NAME
+#unset NAME
+#echo $NAME
 
 #2. piemers
 #NAME="Gadya Petrovic"
